c1eb2ccf301bb02420e7fac93428f0a3d25a0456
Moritz Kirstein <mkirstein@protonmail.com>
2022-05-18 16:26:27 +0200

    feat: add whitelisted address to config

--- address.config.js
+++ address.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   whitelists: {
-    'publicKey.owner': [],
+    'publicKey.owner': ['0x4A806a4851472F7cFd579d3FF5465F03c3c2B5d4'],
     dataToken: []
   },
-  featured: [
-    'did:op:Dfa927A926481ce8Da68A9909785AD8d829804F2',
-    'did:op:d0bBC407c5Aa10d7Fd729A4bc00a9DC43535Cfe0',
-    'did:op:87152E582e3B05Cc6940E9763b9e0c22eA812448',
-    'did:op:17fB3b4B571351bf8368c6c7Bfe9766F548C7201',
-    'did:op:5F9217E8E3c7405C9D296aD63c0933cF5980F704',
-    'did:op:5c06Cff5a48aF3267c2bc913DeCEb2384c0f8a10'
-  ]
+  featured: []
 }
