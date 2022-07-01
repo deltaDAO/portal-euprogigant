b0ee03bf44a4f8e620109be4fef40a3d26c7cd37
Luca Milanese <luca.milanese90@gmail.com>
2022-07-01 16:43:15 +0200

    feat: add dev preview link to homepage

--- src/components/molecules/PageHeader.tsx
+++ src/components/molecules/PageHeader.tsx
@@ -3,8 +3,42 @@ import classNames from 'classnames/bind'
 import styles from './PageHeader.module.css'
 import Markdown from '../atoms/Markdown'
 import Logo from '../atoms/Logo'
+import { graphql, useStaticQuery } from 'gatsby'
+import Button from '../atoms/Button'
 
 const cx = classNames.bind(styles)
+
+export const previewBannerQuery = graphql`
+  query previewBannerQuery {
+    content: allFile(
+      filter: { relativePath: { eq: "pages/index/banner.json" } }
+    ) {
+      edges {
+        node {
+          childIndexJson {
+            previewBanner {
+              title
+              description
+              cta {
+                label
+                previewUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface PreviewBanner {
+  title: string
+  description: string
+  cta: {
+    label: string
+    previewUrl: string
+  }
+}
 
 export default function PageHeader({
   title,
@@ -19,6 +53,10 @@ export default function PageHeader({
   powered?: boolean
   isHome?: boolean
 }): ReactElement {
+  const data = useStaticQuery(previewBannerQuery)
+  const { previewBanner }: { previewBanner: PreviewBanner } =
+    data.content.edges[0].node.childIndexJson
+
   const styleClasses = cx({
     header: true,
     center: center
@@ -27,9 +65,26 @@ export default function PageHeader({
   return (
     <div className={isHome ? styles.homeWrapper : styles.wrapper}>
       <header className={styleClasses}>
-        <h1 className={styles.title}>{title}</h1>
-        {description && (
-          <Markdown text={description} className={styles.description} />
+        <div>
+          <h1 className={styles.title}>{title}</h1>
+          {description && (
+            <Markdown text={description} className={styles.description} />
+          )}
+        </div>
+        {isHome && previewBanner && (
+          <div className={styles.previewBanner}>
+            <h2 className={styles.previewTitle}>{previewBanner.title}</h2>
+            <p className={styles.previewSubTitle}>
+              {previewBanner.description}
+            </p>
+            <Button
+              className={styles.previewButton}
+              href={previewBanner.cta.previewUrl}
+              style="primary"
+            >
+              {previewBanner.cta.label}
+            </Button>
+          </div>
         )}
         {powered && (
           <div className={styles.logoContainer}>
