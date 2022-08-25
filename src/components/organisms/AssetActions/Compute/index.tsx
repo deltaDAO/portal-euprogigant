41092497dfdf494ce9c842110b93701c0c434859
Luca Milanese <luca.milanese90@gmail.com>
2022-08-25 12:22:05 +0200

    fix: aquarius id queries

--- src/components/organisms/AssetActions/Compute/index.tsx
+++ src/components/organisms/AssetActions/Compute/index.tsx
@@ -149,7 +149,7 @@ export default function Compute({
       sort: { sortBy: SortTermOptions.Created },
       filters: [
         getFilterTerm('service.attributes.main.type', 'algorithm'),
-        getFilterTerm('id', algorithmDidList)
+        getFilterTerm('_id', algorithmDidList)
       ]
     } as BaseQueryParams
 
