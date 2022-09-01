6299e4cff975aae637259d4d664c6f28aa521275
Luca Milanese <luca.milanese90@gmail.com>
2022-09-01 16:39:50 +0200

    enhancement: move metadata cache uri to chains config

--- chains.config.js
+++ chains.config.js
@@ -10,6 +10,7 @@ const chains = [
     networkId: GX_NETWORK_ID,
     nodeUri: 'https://rpc.gaiaxtestnet.oceanprotocol.com/',
     providerUri: 'https://ctd.ptw.tu-darmstadt.euprogigant.io',
+    metadataCacheUri: 'https://aquarius.euprogigant.io',
     explorerUri: 'https://blockscout.gaiaxtestnet.oceanprotocol.com/',
     isDefault: true
   }
