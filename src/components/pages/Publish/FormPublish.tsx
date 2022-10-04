c48860b5086626c1071d4d5a9153cbdd02615fa1
Luca Milanese <luca.milanese90@gmail.com>
2022-10-04 11:11:44 +0200

    fix: metadata confirmation checkbox state after reset

--- src/components/pages/Publish/FormPublish.tsx
+++ src/components/pages/Publish/FormPublish.tsx
@@ -102,7 +102,9 @@ export default function FormPublish(): ReactElement {
     field: FormFieldProps
   ) {
     const value =
-      field.type === 'terms' ? !JSON.parse(e.target.value) : e.target.value
+      field.type === 'checkbox' || field.type === 'terms'
+        ? !JSON.parse(e.target.value)
+        : e.target.value
 
     if (field.name === 'access' && value === 'Compute') {
       setComputeTypeSelected(true)
