d187477c1d8717d97583bb6c54da033f90c0dcb5
Luca Milanese <luca.milanese90@gmail.com>
2022-11-03 15:28:04 +0100

    feat: update node uri and active networks

--- chains.config.js
+++ chains.config.js
@@ -12,6 +12,7 @@ const chains = [
     factoryAddress: '0x2720d405ef7cDC8a2E2e5AeBC8883C99611d893C',
     fixedRateExchangeAddress: '0x3c21a90599b5B7f37014cA5Bf30d3f1b73d7e391',
     gasFeeMultiplier: 1,
+    isActive: false,
     isDefault: false,
     metadataCacheUri: 'https://aquarius.euprogigant.io',
     metadataContractAddress: '0xCfDdA22C9837aE76E0faA845354f33C62E03653a',
@@ -36,13 +37,14 @@ const chains = [
     factoryAddress: '0x325c09E2093C56AbDc86c5ccD68c77952e8034Af',
     fixedRateExchangeAddress: '0x69Df9594E6A30a5751D170093059E7adb3Bf5e92',
     gasFeeMultiplier: 1,
+    isActive: true,
     isDefault: true,
     metadataCacheUri: 'https://aquarius.delta-dao.com/',
     metadataContractAddress: '0xfA89407778041EF51B9e1aA16Ff85bDf908D17e6',
     name: 'GEN-X',
     network: 'genxtestnet',
     networkId: GEN_X_NETWORK_ID,
-    nodeUri: 'http://194.182.169.98:8545/',
+    nodeUri: 'https://rpc.genx.minimal-gaia-x.eu',
     oceanTokenAddress: '0x0995527d3473b3A98C471f1ED8787ACD77fBF009',
     oceanTokenSymbol: 'OCEAN',
     providerUri: 'https://provider.genx.delta-dao.com',
@@ -56,11 +58,13 @@ const chains = [
 ]
 
 const getDefaultChainIds = () => {
-  return chains.filter((chain) => chain.isDefault).map((c) => c.chainId)
+  return chains
+    .filter((chain) => chain.isDefault && chain.isActive)
+    .map((c) => c.chainId)
 }
 
 const getSupportedChainIds = () => {
-  return chains.map((c) => c.chainId)
+  return chains.filter((chain) => chain.isActive).map((c) => c.chainId)
 }
 
 module.exports = {
