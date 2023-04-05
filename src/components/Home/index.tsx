df5810ed42f02b56e6c913b9f2c54664f958ec8e
Luca Milanese <luca.milanese90@gmail.com>
2023-04-05 15:43:42 +0200

    refactor: remove unused home sections

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,109 +1,11 @@
-import React, { ReactElement, useEffect, useState } from 'react'
-import Button from '@shared/atoms/Button'
-import { generateBaseQuery, getFilterTerm } from '@utils/aquarius'
-import { useUserPreferences } from '@context/UserPreferences'
-import { SortTermOptions } from '../../@types/aquarius/SearchQuery'
-import SectionQueryResult from './SectionQueryResult'
-import styles from './index.module.css'
-import { useAddressConfig } from '@hooks/useAddressConfig'
-import TopSales from './TopSales'
-import TopTags from './TopTags'
+import React, { ReactElement } from 'react'
 import HomeContent from './Content'
 import OnboardingSection from '@components/@shared/Onboarding'
 
-interface FeaturedSection {
-  title: string
-  query: SearchQuery
-}
-
-function AllAssetsButton(): ReactElement {
-  return (
-    <Button
-      className={styles.allAssetsButton}
-      style="text"
-      to="/search?sort=nft.created&sortOrder=desc"
-      arrow
-    >
-      All datasets and algorithms
-    </Button>
-  )
-}
-
 export default function HomePage(): ReactElement {
-  const { chainIds } = useUserPreferences()
-  const { featured, hasFeaturedAssets } = useAddressConfig()
-
-  const [queryFeatured, setQueryFeatured] = useState<FeaturedSection[]>([])
-  const [queryRecent, setQueryRecent] = useState<SearchQuery>()
-  const [queryMostSales, setQueryMostSales] = useState<SearchQuery>()
-
-  useEffect(() => {
-    const baseParams = {
-      chainIds,
-      esPaginationOptions: {
-        size: 6
-      },
-      sortOptions: {
-        sortBy: SortTermOptions.Created
-      } as SortOptions
-    } as BaseQueryParams
-
-    const baseParamsSales = {
-      chainIds,
-      esPaginationOptions: {
-        size: 6
-      },
-      sortOptions: {
-        sortBy: SortTermOptions.Orders
-      } as SortOptions
-    } as BaseQueryParams
-
-    setQueryRecent(generateBaseQuery(baseParams))
-    setQueryMostSales(generateBaseQuery(baseParamsSales))
-
-    if (hasFeaturedAssets()) {
-      const featuredSections = featured.map((section) => ({
-        title: section.title,
-        query: generateBaseQuery({
-          ...baseParams,
-          esPaginationOptions: {
-            size: section.assets.length
-          },
-          filters: [getFilterTerm('_id', section.assets)]
-        })
-      }))
-
-      setQueryFeatured(featuredSections)
-    }
-  }, [chainIds, featured, hasFeaturedAssets])
-
   return (
     <>
       <OnboardingSection />
-      {hasFeaturedAssets() && (
-        <>
-          {queryFeatured.map((section, i) => (
-            <SectionQueryResult
-              key={`${section.title}-${i}`}
-              title={section.title}
-              query={section.query}
-            />
-          ))}
-          <AllAssetsButton />
-        </>
-      )}
-      <SectionQueryResult
-        title="Recently Published"
-        query={queryRecent}
-        action={<AllAssetsButton />}
-      />
-      <SectionQueryResult
-        title="Most Sales"
-        query={queryMostSales}
-        action={<AllAssetsButton />}
-      />
-      <TopSales title="Publishers With Most Sales" />
-      <TopTags title="Top Tags By Sales" />
       <HomeContent />
     </>
   )
