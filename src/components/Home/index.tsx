f329c1ce0286a17070a2f67558f6f52b06977744
Luca Milanese <luca.milanese90@gmail.com>
2023-04-05 16:56:17 +0200

    feat: add home featured section

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,14 @@
 import React, { ReactElement } from 'react'
 import HomeContent from './Content'
 import OnboardingSection from '@components/@shared/Onboarding'
+import FeaturedAssets from './FeaturedAssets'
 
 export default function HomePage(): ReactElement {
   return (
     <>
       <OnboardingSection />
       <HomeContent />
+      <FeaturedAssets />
     </>
   )
 }
