7f67cb3b325489b15b684fa05d9531788f926bc8
Luca Milanese <luca.milanese90@gmail.com>
2023-04-06 09:26:59 +0200

    feat: update address config

--- address.config.js
+++ address.config.js
@@ -3,7 +3,9 @@ module.exports = {
     'nft.owner': [
       '0x4A806a4851472F7cFd579d3FF5465F03c3c2B5d4',
       '0x21CF19e1FaF3A62f82B432f82152e8c5C0FdBdaF',
-      '0x9dfbda23b65efB1c836828D74a96eB8528A60f3C'
+      '0x9dfbda23b65efB1c836828D74a96eB8528A60f3C',
+      '0xb2AF8b92bFaC5299Cb6EDEf16150BFD1d4d26a93',
+      '0x3dB4E0b1fC6072271BF51e9a0CC17E3c7C4C99f5'
     ],
     'datatokens.address': []
   },
