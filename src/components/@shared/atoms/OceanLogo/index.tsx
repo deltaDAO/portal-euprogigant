07e736b7e343f605125596842aff8f53fab7a4d0
Luca Milanese <luca.milanese90@gmail.com>
2023-04-17 11:30:28 +0200

    feat: update missing nft image default style

--- src/components/@shared/atoms/OceanLogo/index.tsx
+++ src/components/@shared/atoms/OceanLogo/index.tsx
@@ -2,15 +2,27 @@ import React, { ReactElement } from 'react'
 import LogoAssetFull from '@oceanprotocol/art/logo/logo.svg'
 import LogoAsset from '@images/logo.svg'
 import styles from './index.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 export interface LogoProps {
   noWordmark?: boolean
+  white?: boolean
 }
 
-export default function OceanLogo({ noWordmark }: LogoProps): ReactElement {
+export default function OceanLogo({
+  noWordmark,
+  white
+}: LogoProps): ReactElement {
+  const styleClasses = cx({
+    logo: true,
+    white
+  })
+
   return noWordmark ? (
-    <LogoAsset className={styles.logo} />
+    <LogoAsset className={styleClasses} />
   ) : (
-    <LogoAssetFull className={styles.logo} />
+    <LogoAssetFull className={styleClasses} />
   )
 }
