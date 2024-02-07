050734cf7ee2c5f481ba957b49b9dbf0f2c6568c
Moritz Kirstein <mkirstein@protonmail.com>
2024-02-07 16:39:37 +0100

    fix: remove GEN_X_NETWORK references

--- src/@utils/onboarding.ts
+++ src/@utils/onboarding.ts
@@ -1,4 +1,4 @@
-import { GEN_X_NETWORK_ID } from '../../chains.config'
+import { getSupportedChainIds } from '../../chains.config'
 
 interface IErrorParams {
   accountId: string
@@ -14,8 +14,8 @@ const getErrorMessage = ({
   if (!accountId || !web3Provider) {
     return 'Looks like your account is not connected to the portal, please go back to the "Connect" step.'
   }
-  if (networkId !== GEN_X_NETWORK_ID) {
-    return 'Looks like you are not connected to the GEN-X Testnet, please go back to the "Network" step.'
+  if (!getSupportedChainIds().includes(networkId)) {
+    return 'Looks like you are not connected to a supported network, please go back to the "Network" step.'
   }
 
   return 'Looks like something went wrong, please try again.'
